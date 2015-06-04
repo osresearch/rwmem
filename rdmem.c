b6555ff88fb50a4ea11262b6dd98fc636bbeef9e
Trammell Hudson <hudson@trmm.net>
2015-06-04 13:48:43 -0400

    add status updates

--- rdmem.c
+++ rdmem.c
@@ -6,6 +6,7 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <stdlib.h>
+#include <inttypes.h>
 #include <unistd.h>
 #include "DirectHW.h"
 
@@ -21,15 +22,15 @@ main(
 		return EXIT_FAILURE;
 	}
 
-	uintptr_t addr = strtoul(argv[1], NULL, 0);
-	size_t len = strtoul(argv[2], NULL, 0);
+	const uintptr_t addr = strtoul(argv[1], NULL, 0);
+	const size_t len = strtoul(argv[2], NULL, 0);
 
 	// align to a page boundary
 	const uintptr_t page_mask = 0xFFF;
-	uintptr_t page_offset = addr & page_mask;
-	addr &= ~page_mask;
+	const uintptr_t page_offset = addr & page_mask;
+	const uintptr_t map_addr = addr & ~page_mask;
 
-	size_t map_len = (len + page_offset + page_mask) & ~page_mask;
+	const size_t map_len = (len + page_offset + page_mask) & ~page_mask;
 
 	if (iopl(0) < 0)
 	{
@@ -37,7 +38,7 @@ main(
 		return EXIT_FAILURE;
 	}
 
-	const uint8_t * const buf = map_physical(addr, map_len);
+	const uint8_t * const buf = map_physical(map_addr, map_len);
 	if (buf == NULL)
 	{
 		perror("mmap");
@@ -48,7 +49,16 @@ main(
 	size_t offset = 0;
 	while (offset < len)
 	{
-		ssize_t rc = write(STDOUT_FILENO, buf + page_offset + offset, len - offset);
+		size_t chunk = 0x10000;
+		if (chunk > len - offset)
+			chunk = len - offset;
+
+		const void * const virt_addr = buf + page_offset + offset;
+		const uintptr_t phys_addr = map_addr + page_offset + offset;
+
+		fprintf(stderr, "%016"PRIxPTR"\n", phys_addr);
+
+		ssize_t rc = write(STDOUT_FILENO, virt_addr, chunk);
 		if (rc <= 0)
 		{
 			perror("write");
